36bd4419e375f88a4f93e84d376ad878c5ff0243
madhuvenkatk <62830295+madhuvenkatk@users.noreply.github.com>
2022-01-18 21:41:04 +0530

    Add files via upload

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+  
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            Pastries
+          </NavLink>
+          <NavLink to='/cakes' activeStyle>
+            Cakes
+          </NavLink>
+          <NavLink to='/category3' activeStyle>
+            Tarts
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/checkout'>Check Out</NavBtnLink>
+          <NavBtnLink to='/storeinfo'>Store Info</NavBtnLink>
+        </NavBtn>
+        
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
